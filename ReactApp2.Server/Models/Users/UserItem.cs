c61ebc9cd72c81071dd25347692a36799ca5c518
zyzzlegiggle <112403713+zyzzlegiggle@users.noreply.github.com>
2024-11-08 03:06:00 +0800

    setup identity

---
+++ ReactApp2.Server/Models/Users/UserItem.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ReactApp2.Server.Models.Users;
+
+public class UserItem: IdentityUser
+{
+}
--- ReactApp2.Server/Program.cs
+++ ReactApp2.Server/Program.cs
@@ -1,15 +1,35 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ReactApp2.Server;
+using ReactApp2.Server.Models;
+using ReactApp2.Server.Models.Users;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<BookmarkDb>(opt =>
-    opt.UseInMemoryDatabase("BookmarkList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// db contexts
+/*
+var BookmarkDbString = builder.Configuration.GetConnectionString("BookmarkDb") ?? throw new InvalidOperationException("No connection string BookmarkDb");
+builder.Services.AddDbContext<BookmarkDb>(opt =>
+    opt.UseSqlServer(BookmarkDbString));
+
+var UserDbString = builder.Configuration.GetConnectionString("UserDb") ?? throw new InvalidOperationException("No connection string UserDb");
+builder.Services.AddDefaultIdentity<UserItem>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<UserDbContext>();
+builder.Services.AddDbContext<UserDbContext>(
+    options => options.UseSqlServer(UserDbString));
+*/
+var ApplicationDbContextConnection = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("No connection string ApplicationDbContextConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseInMemoryDatabase("ApplicationDbContextConnection"));
+
+// identity
+builder.Services.AddIdentityApiEndpoints<UserItem>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // logging
 builder.Logging.ClearProviders();
@@ -26,6 +46,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// identity
+app.MapIdentityApi<UserItem>();
 
 app.UseHttpsRedirection();
 
